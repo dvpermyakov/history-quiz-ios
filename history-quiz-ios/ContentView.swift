18821e2f671df2f7d904e63d6883eadbc051072a
dmitrii.permiakov <dmitrii.permiakov@revolut.com>
2020-10-08 20:17:21 +0400

    init

---
+++ history-quiz-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  history-quiz-ios
+//
+//  Created by Dmitrii Permiakov on 10/8/20.
+//  Copyright © 2020 Dmitrii Permiakov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
