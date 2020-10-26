8e8305760ba10dc2f628fb75c9c9bbfedaa3cfc8
Dmitrii Permiakov <dvpermyakov1@gmail.com>
2020-10-26 13:50:15 +0400

    improve MainView.swift

--- history-quiz-ios/main/MainView.swift
+++ history-quiz-ios/main/MainView.swift
@@ -6,18 +6,60 @@
 import SwiftUI
 
 struct MainView: View {
+    private var tabs = [MainTab.Periods, MainTab.Balance]
+
     var body: some View {
-        TabView {
-            Router.createPeriods()
-                    .tabItem {
-                        Image(systemName: "list.dash")
-                        Text("Periods")
-                    }
-            Router.createBalance()
-                    .tabItem {
-                        Image(systemName: "square.and.pencil")
-                        Text("Balance")
-                    }
+        TabView() {
+            ForEach(tabs) { tab in
+                MainTabContentView(tab: tab).tabItem {
+                    Image(systemName: tab.image)
+                    Text(tab.name)
+                }
+            }
+        }
+    }
+}
+
+struct MainTabContentView: View {
+    var tab: MainTab
+
+    var body: some View {
+        Group {
+            switch tab {
+            case .Periods:
+                Router.createPeriods()
+            case .Balance:
+                Router.createBalance()
+            }
+        }
+    }
+
+}
+
+enum MainTab {
+    case Periods
+    case Balance
+}
+
+extension MainTab: Identifiable {
+    var id: String {
+        self.name
+    }
+
+    var name: String {
+        switch self {
+        case .Periods:
+            return "Periods"
+        case .Balance:
+            return "Balance"
+        }
+    }
+    var image: String {
+        switch self {
+        case .Periods:
+            return "list.dash"
+        case .Balance:
+            return "square.and.pencil"
         }
     }
 }
