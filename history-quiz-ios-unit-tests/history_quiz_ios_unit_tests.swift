75c8eee31a22c2c7652cf0b1cd3bf280c9872b9c
Dmitrii Permiakov <dvpermyakov1@gmail.com>
2020-10-26 12:54:11 +0400

    add unit tests environment

---
+++ history-quiz-ios-unit-tests/history_quiz_ios_unit_tests.swift
@@ -0,0 +1,33 @@
+//
+//  history_quiz_ios_unit_tests.swift
+//  history-quiz-ios-unit-tests
+//
+//  Created by Dmitrii Permiakov on 10/26/20.
+//  Copyright © 2020 Dmitrii Permiakov. All rights reserved.
+//
+
+import XCTest
+
+class history_quiz_ios_unit_tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
