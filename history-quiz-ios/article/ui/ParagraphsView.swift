fa6c20baf5895d22fbf234116a2d9f734449fb72
Dmitrii Permiakov <dvpermyakov1@gmail.com>
2020-10-29 22:51:38 +0400

    add image to ParagraphsView.swift

--- history-quiz-ios/article/ui/ParagraphsView.swift
+++ history-quiz-ios/article/ui/ParagraphsView.swift
@@ -15,11 +15,34 @@ struct ParagraphsView: View {
                     Text(paragraph.title).font(Font.system(.body)).bold()
                     Group {
                         if let url = paragraph.image {
-                            UrlImageView(url: url)
+                            ParagraphImageView(url: url)
                         }
                     }
                     Text(paragraph.text).font(Font.system(.body))
                 }.padding()
+            }
+        }
+    }
+}
+
+struct ParagraphImageView: View {
+    @ObservedObject
+    var imageLoader: ImageLoader
+
+    init(url: String) {
+        imageLoader = ImageLoader(url: url)
+    }
+
+    var body: some View {
+        Group {
+            if let data = imageLoader.data, let uiImage = UIImage(data: data) {
+                Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+            } else {
+                Group {
+                    ProgressView()
+                }.frame(maxWidth: .infinity, maxHeight: 100)
             }
         }
     }
