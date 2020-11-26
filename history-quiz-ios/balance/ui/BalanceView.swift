f234d78342ba82bdfd0c7bec7506e4159a1ed0e4
Dmitrii Permiakov <dvpermyakov1@gmail.com>
2020-11-26 20:12:58 +0400

    update balance view

--- history-quiz-ios/balance/ui/BalanceView.swift
+++ history-quiz-ios/balance/ui/BalanceView.swift
@@ -10,24 +10,27 @@ struct BalanceView: View {
     var viewModel: BalanceViewModel
 
     var body: some View {
-        ScrollView {
-            VStack(alignment: HorizontalAlignment.leading) {
-                Text("Balance : \(viewModel.balanceSum)").padding(.top, 25)
-                Text("Way to receive money").padding(.top, 25).padding(.bottom, 10)
-                if (viewModel.receiveBonusesInfo != nil) {
-                    CommonListView(model: viewModel.receiveBonusesInfo!)
-                }
-                Text("Way to spend money").padding(.top, 25).padding(.bottom, 10)
-                if (viewModel.spendBonusesInfo != nil) {
-                    CommonListView(model: viewModel.spendBonusesInfo!)
-                }
-                if (!viewModel.transactions.isEmpty) {
-                    Text("Balance history").padding(.top, 25).padding(.bottom, 10)
-                    ForEach(viewModel.transactions) { transaction in
-                        Text(transaction.title)
+        NavigationView {
+            ScrollView {
+                VStack(alignment: HorizontalAlignment.leading) {
+                    Text("Way to receive money").padding(.bottom, 10)
+                    if (viewModel.receiveBonusesInfo != nil) {
+                        CommonListView(model: viewModel.receiveBonusesInfo!)
                     }
-                }
-            }.padding()
+                    Text("Way to spend money").padding(.top, 25).padding(.bottom, 10)
+                    if (viewModel.spendBonusesInfo != nil) {
+                        CommonListView(model: viewModel.spendBonusesInfo!)
+                    }
+                    if (!viewModel.transactions.isEmpty) {
+                        Text("Balance history")
+                                .padding(.top, 25)
+                                .padding(.bottom, 10)
+                        ForEach(viewModel.transactions) { transaction in
+                            Text(transaction.title)
+                        }
+                    }
+                }.navigationBarTitle("Balance : \(viewModel.balanceSum)").padding()
+            }
         }
     }
 }
