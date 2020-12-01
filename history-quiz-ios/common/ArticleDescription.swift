2e1b6b5d939d2264d14aa5a2f225be7e6a110702
Dmitrii Permiakov <dvpermyakov1@gmail.com>
2020-12-01 14:50:11 +0400

    add article description protocol

---
+++ history-quiz-ios/common/ArticleDescription.swift
@@ -0,0 +1,11 @@
+//
+// Created by Dmitrii Permiakov on 12/1/20.
+// Copyright (c) 2020 Dmitrii Permiakov. All rights reserved.
+//
+
+import Foundation
+
+protocol ArticleDescription {
+    var name: String { get }
+    var description: String { get }
+}
