aa82a0069337fb7d2e4a56ccdb8265e9cd2b2c43
Dmitrii Permiakov <dvpermyakov1@gmail.com>
2020-12-02 00:49:42 +0400

    fix game view

--- history-quiz-ios/game/ui/GameView.swift
+++ history-quiz-ios/game/ui/GameView.swift
@@ -13,7 +13,7 @@ struct GameView: View {
     var viewModel: GameViewModel
 
     var body: some View {
-        NavigationView {
+        Group {
             if let gameResult = viewModel.gameResult {
                 GameResultView(result: gameResult).environmentObject(viewModel)
             } else if let question = viewModel.currentQuestion {
